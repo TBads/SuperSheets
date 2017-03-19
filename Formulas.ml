e56d89ae5509fb058fbeb5a16c554e441e54664d
PamExx <thomas@pamexx.com>
2017-03-18 23:31:57 -0400

    Can now handle parentheses in calculations

--- Formulas.ml
+++ Formulas.ml
@@ -1,12 +1,15 @@
-(* TODO: Need to be fully handle parentheses, exponents, cell references and function calls *)
+(* TODO: Need to be fully handle exponents, cell references and function calls *)
 
-type token =
-  | FormulaBegin
-  | Number of float
+type operator =
   | Add
   | Subtract
   | Multiply
   | Divide
+
+type token =
+  | FormulaBegin
+  | Number of float
+  | Op of operator
   | LeftParen
   | RightParen
   | StringTokenError of string
@@ -14,22 +17,22 @@ type token =
 let token_of_string s =
   match s with
   | "=" -> FormulaBegin
-  | "+" -> Add
-  | "-" -> Subtract
-  | "*" -> Multiply
-  | "/" -> Divide
-  (*| "(" -> LeftParen
-    | ")" -> RightParen*)
+  | "+" -> Op Add
+  | "-" -> Op Subtract
+  | "*" -> Op Multiply
+  | "/" -> Op Divide
+  | "(" -> LeftParen
+  | ")" -> RightParen
   | _   -> try Number (float_of_string s) with _ -> StringTokenError s
 
 let string_of_token =
   function
   | FormulaBegin -> "FormulaBegin"
   | Number n -> "Number" ^ (string_of_float n)
-  | Add -> "Add"
-  | Subtract -> "Subtract"
-  | Multiply -> "Multiply"
-  | Divide -> "Divide"
+  | Op Add -> "Add"
+  | Op Subtract -> "Subtract"
+  | Op Multiply -> "Multiply"
+  | Op Divide -> "Divide"
   | LeftParen -> "LeftParen"
   | RightParen -> "RightParen"
   | StringTokenError s -> ("StringTokenError " ^ s)
@@ -37,7 +40,7 @@ let string_of_token =
 (* Parse a string into a list of strings that will be evaluated as tokens *)
 let parse_string s =
   Str.split (Str.regexp "[ \t]+") s
-  |> List.map (Str.full_split (Str.regexp "[= + /- - /* //]+"))
+  |> List.map (Str.full_split (Str.regexp "[= + /- - /* // ( )]"))
   |> List.flatten
   |> List.map (function | Str.Delim x -> x | Str.Text x -> x)
   |> List.map token_of_string
@@ -62,12 +65,38 @@ let stack_of_list l =
 let stack_of_que q =
   list_of_que q |> stack_of_list
 
+let print_stack s =
+  let printable_stack = Stack.copy s in
+
+  let rec print_items () =
+    try
+      print_endline (string_of_token @@ Stack.pop printable_stack);
+      print_items ()
+    with _ -> print_newline ()
+  in
+
+  print_endline "Stack =";
+  print_items ()
+
 (* Shunting-yard algorithm *)
 let shunting_yard (l : token list) =
 
   let out_que, op_stack = Queue.create (), Stack.create () in
 
-  (* TODO: handling of parentheses *)
+  let rec handle_right_paren () =
+    try
+      if Stack.top op_stack = LeftParen
+      then (
+        let _ = Stack.pop op_stack in
+        ()
+      )
+      else (
+        Queue.push (Stack.pop op_stack) out_que;
+        handle_right_paren ()
+      )
+    with empty -> failwith "Error: Unmatched parentheses"
+  in
+
   let rec run_algo tokens =
     match tokens with
     | [] -> (try (Queue.push (Stack.pop op_stack) out_que; run_algo tokens) with empty -> ())
@@ -76,46 +105,58 @@ let shunting_yard (l : token list) =
         Queue.push (Number n) out_que;
         run_algo tl
       )
-    | Add :: tl -> (
-        try
-          let o2 = Stack.pop op_stack in
-          Queue.push o2 out_que;
-          run_algo tokens
-        with
-          empty -> (Stack.push Add op_stack; run_algo tl)
-      )
-    | Subtract :: tl -> (
-        try
-          let o2 = Stack.pop op_stack in
-          Queue.push o2 out_que;
-          run_algo tokens
-        with
-          empty -> (Stack.push Subtract op_stack; run_algo tl)
-      )
-    | Multiply as o1 :: tl -> (
+    | Op Add as o1 :: tl -> (
         try
           let o2 = Stack.top op_stack in
-          if o2 = Multiply || o2 = Divide
-          then (Queue.push (Stack.pop op_stack) out_que; run_algo tokens)
-          else (Stack.push Multiply op_stack; run_algo tl)
+          match o2 with
+          | Op _ -> (
+              let _ = Stack.pop op_stack in
+              Queue.push o2 out_que;
+              run_algo tokens
+            )
+          | _ -> (Stack.push (Op Add) op_stack; run_algo tl)
         with
-          empty -> (Stack.push Multiply op_stack; run_algo tl)
+          empty -> (Stack.push (Op Add) op_stack; run_algo tl)
       )
-    | Divide as o1 :: tl -> (
+    | Op Subtract :: tl -> (
         try
           let o2 = Stack.top op_stack in
-          if o2 = Multiply || o2 = Divide
-          then (Queue.push (Stack.pop op_stack) out_que; run_algo tokens)
-          else (Stack.push Divide op_stack; run_algo tl)
+          match o2 with
+          | Op _ -> (
+              let _ = Stack.pop op_stack in
+              Queue.push o2 out_que;
+              run_algo tokens
+            )
+          | _ -> (Stack.push (Op Subtract) op_stack; run_algo tl)
         with
-          empty -> (Stack.push Divide op_stack; run_algo tl)
+          empty -> (Stack.push (Op Subtract) op_stack; run_algo tl)
       )
-
-    (*| LeftParen :: tl -> (Stack.push LeftParen op_stack; run_algo tl)
-    | RightParen :: tl ->
-      if Stack.top op_stack = LeftParen
-      then (Queue.push (Stack.pop op_stack) out_que; run_algo tl)
-      else (Queue.push (Stack.pop op_stack) out_que; run_algo tokens)*)
+    | Op Multiply as o1 :: tl -> (
+        try
+          let o2 = Stack.top op_stack in
+          if o2 = (Op Multiply) || o2 = (Op Divide)
+          then (Queue.push (Stack.pop op_stack) out_que; run_algo tokens)
+          else (Stack.push (Op Multiply) op_stack; run_algo tl)
+        with
+          empty -> (Stack.push (Op Multiply) op_stack; run_algo tl)
+      )
+    | Op Divide as o1 :: tl -> (
+        try
+          let o2 = Stack.top op_stack in
+          if o2 = (Op Multiply) || o2 = (Op Divide)
+          then (Queue.push (Stack.pop op_stack) out_que; run_algo tokens)
+          else (Stack.push (Op Divide) op_stack; run_algo tl)
+        with
+          empty -> (Stack.push (Op Divide) op_stack; run_algo tl)
+      )
+    | LeftParen :: tl -> (
+        Stack.push LeftParen op_stack;
+        run_algo tl
+      )
+    | RightParen :: tl -> (
+        handle_right_paren ();
+        run_algo tl
+      )
     | _ -> failwith "Error: shunting_yard"
   in
 
@@ -127,18 +168,6 @@ let float_of_token =
   | Number n -> n
   | _ -> failwith "Error: float_of_token"
 
-let print_stack s =
-  let printable_stack = Stack.copy s in
-
-  let rec print_items () =
-    try
-      print_endline (string_of_token @@ Stack.pop printable_stack);
-      print_items ()
-    with _ -> print_newline ()
-  in
-
-  print_items ()
-
 (* Evaluate a stack of tokens in reverse polish notation *)
 let eval l =
   let (s : token Stack.t) = Stack.create () in
@@ -149,26 +178,26 @@ let eval l =
     | Number n :: tl -> (
         (Stack.push (Number n) s; run tl)
       )
-    | Add :: tl -> (
+    | Op Add :: tl -> (
         let n1 = float_of_token @@ Stack.pop s in
         let n2 = float_of_token @@ Stack.pop s in
         Stack.push (Number (n1 +. n2)) s;
         run tl
       )
-    | Subtract :: tl -> (
+    | Op Subtract :: tl -> (
         let n1 = float_of_token @@ Stack.pop s in
         let n2 = float_of_token @@ Stack.pop s in
         Stack.push (Number (n2 -. n1)) s;
         run tl
       )
 
-    | Multiply :: tl -> (
+    | Op Multiply :: tl -> (
         let n1 = float_of_token @@ Stack.pop s in
         let n2 = float_of_token @@ Stack.pop s in
         Stack.push (Number (n1 *. n2)) s;
         run tl
       )
-    | Divide :: tl -> (
+    | Op Divide :: tl -> (
         let n1 = float_of_token @@ Stack.pop s in
         let n2 = float_of_token @@ Stack.pop s in
         Stack.push (Number (n2 /. n1)) s;
